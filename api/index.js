eb918816d730c0c5e5657597ff67097088286597
Enrico Brambilla <enric.brambilla@gmail.com>
2020-08-28 18:33:47 +0200

    schema validation

--- api/index.js
+++ api/index.js
@@ -5,21 +5,20 @@ const yup = require('yup');
 
 const api = express();
 
-const mongo_url = 'localhost:27017/myproject'; //TODO env variables
-const db = monk(url);
+// const mongo_url = 'localhost:27017/yaus'; //TODO env variables
+// const db = monk(mongo_url);
 
 const schema = yup.object().shape({
     id: yup.string().matches(/^[\w\-]+$/),
     redirect: yup.string().url().required(),
 });
 
-db.then(() => {
-  console.log('Connected correctly to server')
-})
+// db.then(() => {
+//   console.log('Connected correctly to server')
+// })
 
 api.use(express.json());
 api.use(bodyParser.json());
-//api.use(bodyParser.urlencoded()); 
 
 api.get('/urls/:id', (req, res) => {
     const { id } = req.params;
@@ -33,10 +32,26 @@ api.get('/urls/:id', (req, res) => {
     });
 });
 
-api.post('/urls', (req, res) => {
+api.post('/urls', async (req, res) => {
     // TODO create new redirect url
 
-    const { id, redirect } = req.body;
+    var { id, redirect } = req.body;
+
+    console.log({
+        id,
+        redirect,
+    })
+
+    try {
+        await schema.validate({
+            id,
+            redirect,
+        });
+    } catch (error) {
+        return res.json({
+            status: 500,
+        });
+    }
 
     res.json({
         id: id,
