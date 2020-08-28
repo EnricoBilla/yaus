2a10b37b1a53b45e2fd741bf8373c2775d1968c6
Enrico Brambilla <enric.brambilla@gmail.com>
2020-08-28 17:49:05 +0200

    general structure of api, schema validation for new redirect

--- api/index.js
+++ api/index.js
@@ -1,17 +1,49 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const monk = require('monk');
+const yup = require('yup');
 
 const api = express();
 
-api.use(express.json());
+const mongo_url = 'localhost:27017/myproject'; //TODO env variables
+const db = monk(url);
 
-api.get('/', (req, res) => {
+const schema = yup.object().shape({
+    id: yup.string().matches(/[\w]+/),
+    redirect: yup.string().url().required(),
+});
+
+db.then(() => {
+  console.log('Connected correctly to server')
+})
+
+api.use(express.json());
+api.use(bodyParser.json());
+//api.use(bodyParser.urlencoded()); 
+
+api.get('/urls/:id', (req, res) => {
+    const { id } = req.params;
+
+    // TODO give info about redirect to id
     res.json({
-        message: "api is working",
+        id: id,
+        redirect: "https://google.com",
+        clicks: 17,
+        message: "here you go",
     });
 });
 
-// api.get('/:id', (req, res) => {
-//     // TODO give info about redirect to id
-// });
+api.post('/urls', (req, res) => {
+    // TODO create new redirect url
+
+    const { id, redirect } = req.body;
+
+    res.json({
+        id: id,
+        redirect: redirect,
+        message: "created new redirect",
+    });
+
+});
 
 module.exports = api;
