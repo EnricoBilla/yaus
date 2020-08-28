464b2dfa54834483ed8776ab1f218ec419afcaf2
Enrico Brambilla <enric.brambilla@gmail.com>
2020-08-28 22:03:02 +0200

    url redirect

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const monk = require('monk');
+
 const api = require('./api')
+
+const mongo_url = 'localhost:27017/yaus'; //TODO env variables
+const db = monk(mongo_url);
+const urls = db.get('urls');
+urls.createIndex({ id: 1 }, { unique: true });
 
 const app = express();
 const PORT = 3000;
@@ -7,10 +14,17 @@ const PORT = 3000;
 app.use(express.static('./static'))
 app.use('/api', api);
 
-app.get('/:id', (req, res) => {
-    // TODO redirect to the requested url
+app.get('/:id', async (req, res) => {
+
     const { id } = req.params;
-    res.send(`Redirect to ${id}`);
+
+    await urls.findOne({id: id})
+        .then(result => {
+            res.redirect(result.redirect);
+        })
+        .catch(error => {
+            res.redirect('/?error=Redirect not found');
+        });
 });
 
 app.listen(PORT, () => {
